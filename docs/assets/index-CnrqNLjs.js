1756df72312bf0c6ee20e6f9c36c8fb732d50f33
mesud1916 <m.masoud.tr@gmail.com>
2026-01-08 17:24:11 +0300

    rebuild dist as docs for github pages

---
+++ docs/assets/index-CnrqNLjs.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-D46s0Yeo.js","assets/index-BLXExJD-.js"])))=>i.map(i=>d[i]);
+import{r,_ as t}from"./index-BLXExJD-.js";const o=r("Share",{web:()=>t(()=>import("./web-D46s0Yeo.js"),__vite__mapDeps([0,1])).then(e=>new e.ShareWeb)});export{o as Share};
